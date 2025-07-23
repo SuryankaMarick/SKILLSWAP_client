2010112a76866802d79ba73eede27cde10e71b57
Suryanka Marick <surya9088907993@gmail.com>
2025-07-24 02:48:00 +0530

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import styles from '../../styles/Home.module.css'
+
+const Home = () => {
+  return (
+    <div id={styles.main}>
+      <div id={styles.header}>
+        <div id={styles.title}>
+          <p>SkillSwap</p>
+        </div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
